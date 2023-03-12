a580349c4fa97f0055fc9560551d85974fe878d6
Fanomez4ntsoa <87415578+Fanomez4ntsoa@users.noreply.github.com>
2023-03-12 21:25:55 +0300

    wip: set up Layout & header & footer

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <>
+      <header className='header-top-strip py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <p className='text-white mb-0'> Free Shipping Over $100 & Free Returns</p>
+            </div>
+            <div className='col-6'>
+              <p className='text-end text-white mb-0'>
+                Hotline: <a className='text-white' href='tel: +261 38 79 205 85'> +261 38 79 205 85 </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className='header-upper py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-2'>
+              <h1> 
+                <Link> Developers.</Link>
+              </h1>
+            </div>
+            <div className='col-5'></div>
+            <div className='col-5'></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
